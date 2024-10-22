d1ed557fcb4148457673e13224fbae0857dc411f
Ulthran <WladimirZhirinovsky@gmail.com>
2024-10-22 16:18:37 +0300

    Add Plugin

---
+++ EmployeeRestLoader/EmployeeRestLoaderPlugin.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System.Net;
+using PhoneApp.Domain.Attributes;
+
+namespace EmployeeRestLoader
+{
+    [Author(Name = "IHopeThisIs AllIntentional")]
+    public class Plugin : IPluggable
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info("Loading users");
+            var usersList = args.Cast<EmployeesDTO>().ToList();
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+                                    | SecurityProtocolType.Tls11
+                                    | SecurityProtocolType.Tls
+                                    | SecurityProtocolType.Ssl3;
+
+            var json = new WebClient().DownloadString("https://dummyjson.com/users");
+
+            var parsedJson = Newtonsoft.Json.JsonConvert.DeserializeObject<Users>(json);
+            
+
+
+            foreach(UserDTO user in parsedJson.users)
+            {
+                logger.Info(user);
+                EmployeesDTO employee = new EmployeesDTO();
+                employee.Name = user.FirstName + " " + user.LastName;
+                employee.AddPhone(user.PhoneNumber);
+                usersList.Add(employee);
+            }
+            
+            logger.Info($"Loaded {usersList.Count()} employees");
+            return usersList.Cast<DataTransferObject>();
+        }
+    }
+}
---
+++ EmployeeRestLoader/UsersDTO.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace EmployeeRestLoader
+{
+    internal class UserDTO 
+    {
+        [JsonPropertyAttribute("firstName")]
+        public string FirstName { get; set; }
+        [JsonPropertyAttribute("lastName")]
+        public string LastName { get; set; }
+        [JsonPropertyAttribute("phone")]
+        public string PhoneNumber { get; set; }
+    }
+
+    internal class Users
+    {
+        [JsonPropertyAttribute("users")]
+        public List<UserDTO> users;
+    }
+}
